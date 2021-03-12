6826f8169d41d90d56cfeff728a8705c5ea0e4f8
Skipper <46247235+SzymonFaligowski@users.noreply.github.com>
2021-03-12 01:12:21 +0100

    v0.0.2

--- src/background.js
+++ src/background.js
@@ -3,10 +3,15 @@ const socket = io('http://localhost:5000');
 console.log('Hello fucker');
 
 let currentTest;
+// document.querySelector('#h').innerText = 'Siema byczku'
 
-socket.on('connect',()=>{
-    console.log('Hello');
-})
+socket.on('connect', () => {
+  console.log('Hello');
+});
+
+socket.on('answers', (answ) => {
+  console.log(answ);
+});
 
 chrome.runtime.onMessage.addListener((req, sender, send) => {
   if (req.status === 'setTestID') {
